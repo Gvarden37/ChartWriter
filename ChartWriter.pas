b39fec0f05ec896523e1ee424df944113b7a8678
Gvarden37 <oivind.muller@gmail.com>
2023-05-01 20:15:24 +0200

    removed r

--- ChartWriter.pas
+++ ChartWriter.pas
@@ -1,6 +1,6 @@
 {**************************************************************************** }
 { Version 1.0.0 2022 }
-{ Author: Oivind Mullerr }
+{ Author: Oivind Muller }
 { }
 { This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
